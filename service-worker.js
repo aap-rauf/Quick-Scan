f4ed9351947efeea09dc13235aacf0ef6a863ac6
Rauf <raufhossain548@gmail.com>
2025-11-20 06:45:12 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE = "easy-scan-v100";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key =>
+        key !== CACHE ? caches.delete(key) : null
+      ))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(e.request).then(resp => {
+        if (!resp || resp.status !== 200) return resp;
+        const copy = resp.clone();
+
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+
+        return resp;
+      });
+    })
+  );
+});
