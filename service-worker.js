e4459b131b7e432f2199547c5d35cd87991c38bb
Rauf <raufhossain548@gmail.com>
2026-06-11 05:07:34 +0530

    Refactor service worker to simplify installation
    Removed asset caching and fetch handling logic.

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,9 @@
 const CACHE = "easy-scan-v1.0.3";
 
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener("message", event => {
-  if (event.data?.action === "skipWaiting") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const clone = response.clone();
-
-        caches.open(CACHE).then(cache => {
-          cache.put(event.request, clone);
-        });
-
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.waitUntil(self.clients.claim());
 });
